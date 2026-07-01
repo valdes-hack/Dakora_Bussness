38e178be9a2d0fdc5e4e5782683e77ae6b2bf399
valdes-hack <valdeshacking01@gmail.com>
2026-07-01 06:19:51 +0100

    implementation des villes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ export default defineConfig({
     // Configuration PWA ultra-pro
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon/favicon.ico', 'icon/apple-touch-icon.png', 'icon/android-icon-192x192.png', 'icon/android-icon-144x144.png', 'icon/ms-icon-310x310.png'],
+      includeAssets: [
+        'icon/favicon.ico',
+        'icon/apple-touch-icon.png',
+        'icon/android-icon-192x192.png',
+        'icon/android-icon-144x144.png',
+        'icon/ms-icon-310x310.png'
+      ],
       manifest: {
         name: 'Dakora Business',
         short_name: 'Dakora',
@@ -21,13 +27,23 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: 'icon/android-icon-192x192.png',
-            sizes: '192x192',
+            src: 'icon/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icon/android-icon-96x96.png',
+            sizes: '96x96',
             type: 'image/png'
           },
           {
             src: 'icon/android-icon-144x144.png',
             sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icon/android-icon-192x192.png',
+            sizes: '192x192',
             type: 'image/png'
           },
           {
